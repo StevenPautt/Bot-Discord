1153ce4e22324f70bb61cc845fd4b587f313431f
StevenPautt <steven2000.p@hotmail.com>
2023-07-31 18:08:35 -0500

    Main

--- Commands/Public/ApiChek.js
+++ Commands/Public/ApiChek.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
 const db = require('./db');
+const Excel = require('exceljs');
 
 const apiKey = '63b09a153d4b4bec80be95f0f1e559ae';
 
@@ -52,20 +53,38 @@ module.exports = {
           `Autonomous System Organization (ASO): ${data?.network?.autonomous_system_organization ?? 'No disponible'}`
         ];
 
-        const replyMessage = formattedData.join('\n');
+        // Verificar si el IP o el nickname ya están en la base de datos
+        const query = 'SELECT * FROM usuarios WHERE ip = ? OR nickname = ?';
+        const checkValues = [ipAddress, nickname];
 
-        const sqlQuery = `INSERT INTO usuarios (nickname, ip, es_vpn, es_proxy, es_tor, pais) VALUES (?, ?, ?, ?, ?, ?)`;
-        const insertValues = [nickname, ipAddress, esVPN, esProxy, esTOR, data?.location?.country ?? 'No disponible'];
-
-        db.query(sqlQuery, insertValues, (err, insertResult) => {
+        db.query(query, checkValues, (err, checkResults) => {
           if (err) {
-            console.error('Error al insertar en la base de datos:', err);
-            interaction.reply('Error al insertar en la base de datos.');
+            console.error('Error al consultar la base de datos:', err);
+            interaction.reply('Ocurrió un error al consultar la base de datos.');
             return;
           }
 
-          const successMessage = `\nInformación agregada a la base de datos:\n${replyMessage}`;
-          interaction.reply(successMessage);
+          if (checkResults.length > 0) {
+            // Ya existe una entrada con el mismo IP o nickname
+            const existingData = checkResults.map(entry => `Nickname: ${entry.nickname}, IP: ${entry.ip}`);
+            const replyMessage = [...formattedData, '\n\nYa existe una entrada con el mismo IP o nickname:', ...existingData].join('\n');
+            interaction.reply(replyMessage);
+          } else {
+            // No existe una entrada con el mismo IP o nickname, insertar los datos en la base de datos
+            const sqlQuery = `INSERT INTO usuarios (nickname, ip, es_vpn, es_proxy, es_tor, pais) VALUES (?, ?, ?, ?, ?, ?)`;
+            const insertValues = [nickname, ipAddress, esVPN, esProxy, esTOR, data?.location?.country ?? 'No disponible'];
+
+            db.query(sqlQuery, insertValues, (insertErr, insertResult) => {
+              if (insertErr) {
+                console.error('Error al insertar en la base de datos:', insertErr);
+                interaction.reply('Error al insertar en la base de datos.');
+                return;
+              }
+
+              const successMessage = `\nInformación agregada a la base de datos:\n${formattedData.join('\n')}`;
+              interaction.reply(successMessage);
+            });
+          }
         });
       } else {
         interaction.reply(`Error en la solicitud. Código de error: ${response.status}`);
