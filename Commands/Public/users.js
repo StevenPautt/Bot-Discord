53dc42fa89c9e1cb4e4130860dc2ffc8561f8c9e
StevenPautt <steven2000.p@hotmail.com>
2023-07-31 17:28:06 -0500

    Main

--- Commands/Public/users.js
+++ Commands/Public/users.js
@@ -1,16 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 const db = require('./db');
-const Table = require('table');
+const Excel = require('exceljs');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('users')
-    .setDescription('Ver los usuarios e IPs guardados en la base de datos'),
+    .setDescription('Exportar los usuarios e IPs a un archivo Excel'),
   async execute(interaction) {
     try {
       // Consultar la base de datos para obtener los usuarios e IPs
       const query = 'SELECT nickname, ip FROM usuarios';
-      db.query(query, (err, results) => {
+      db.query(query, async (err, results) => {
         if (err) {
           console.error('Error al consultar la base de datos:', err);
           interaction.reply('Ocurrió un error al consultar la base de datos.');
@@ -23,27 +23,31 @@ module.exports = {
           return;
         }
 
-        // Crear la tabla con los resultados de la consulta
-        const data = [['Usuario', 'IP']];
-        for (const entry of results) {
-          data.push([entry.nickname, entry.ip]);
-        }
-        const config = {
-          columns: {
-            0: {
-              alignment: 'left',
-              width: 30,
-            },
-            1: {
-              alignment: 'left',
-              width: 15,
-            },
-          },
-        };
-        const table = Table.table(data, config);
+        // Crear un nuevo libro y una nueva hoja de cálculo en el archivo Excel
+        const workbook = new Excel.Workbook();
+        const worksheet = workbook.addWorksheet('Usuarios e IPs');
 
-        // Responder con la tabla formateada
-        interaction.reply('Usuarios e IPs en la base de datos:\n```' + table + '```');
+        // Definir los encabezados de la tabla en el archivo Excel
+        worksheet.columns = [
+          { header: 'Usuario', key: 'nickname', width: 30 },
+          { header: 'IP', key: 'ip', width: 15 },
+        ];
+
+        // Agregar los datos de la base de datos a la tabla en el archivo Excel
+        for (const entry of results) {
+          worksheet.addRow({ nickname: entry.nickname, ip: entry.ip });
+        }
+
+        // Generar el archivo Excel
+        const buffer = await workbook.xlsx.writeBuffer();
+
+        // Responder con el archivo Excel como adjunto
+        interaction.reply({
+          files: [{
+            attachment: buffer,
+            name: 'usuarios_ips.xlsx', // Puedes cambiar el nombre del archivo si lo deseas
+          }],
+        });
       });
     } catch (error) {
       interaction.reply(`Error al leer la base de datos: ${error.message}`);
