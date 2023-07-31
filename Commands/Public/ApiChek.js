e18f18078391111077638ad24fec838332c2e258
StevenPautt <steven2000.p@hotmail.com>
2023-07-31 14:38:44 -0500

    x2

--- Commands/Public/ApiChek.js
+++ Commands/Public/ApiChek.js
@@ -3,12 +3,10 @@ const axios = require('axios');
 const db = require('./db');
 const { table } = require('table');
 
-// Reemplaza 'YOUR_API_KEY' con tu clave de API
 const apiKey = '63b09a153d4b4bec80be95f0f1e559ae';
 
-// Función para traducir true/false a SI⚠️ o NO
 function traducirBoolean(valor) {
-  return valor ? 'SI' : 'NO';
+  return valor ? 'SI ⚠️' : 'NO';
 }
 
 module.exports = {
@@ -33,43 +31,35 @@ module.exports = {
     try {
       const response = await axios.get(url);
 
-      // Verificar si la solicitud fue exitosa (código 200)
       if (response.status === 200) {
         const data = response.data;
 
-        // Obtener los valores SI/NO para VPN, Proxy y TOR
         const esVPN = traducirBoolean(data?.security?.vpn);
         const esProxy = traducirBoolean(data?.security?.proxy);
         const esTOR = traducirBoolean(data?.security?.tor);
 
-        // Guardar los datos en la base de datos
-        const insertQuery = `INSERT INTO usuarios (nickname, ip, pais, es_vpn, es_proxy, es_tor) VALUES (?, ?, ?, ?, ?, ?)`;
-        const insertValues = [nickname, ipAddress, data?.location?.country ?? 'No disponible', esVPN, esProxy, esTOR];
-        db.query(insertQuery, insertValues, (err, insertResult) => {
+        const outputTable = [
+          ['Información sobre la dirección IP', ipAddress],
+          ['¿Es una VPN?', esVPN],
+          ['¿Es un proxy?', esProxy],
+          ['¿Es TOR?', esTOR],
+          ['País', data?.location?.country ?? 'No disponible']
+        ];
+
+        const output = table(outputTable);
+
+        const sqlQuery = `INSERT INTO usuarios (nickname, ip, es_vpn, es_proxy, es_tor, pais) VALUES (?, ?, ?, ?, ?, ?)`;
+        const insertValues = [nickname, ipAddress, esVPN, esProxy, esTOR, data?.location?.country ?? 'No disponible'];
+
+        db.query(sqlQuery, insertValues, (err, insertResult) => {
           if (err) {
             console.error('Error al insertar en la base de datos:', err);
             interaction.reply('Error al insertar en la base de datos.');
             return;
           }
 
-          // Mostrar el mensaje con la información sobre la dirección IP
-          const tableData = [
-            ['Información sobre la dirección IP', ipAddress],
-            ['¿Es una VPN?', esVPN],
-            ['¿Es un proxy?', esProxy],
-            ['¿Es TOR?', esTOR],
-            ['País', data?.location?.country ?? 'No disponible'],
-            ['Continente', data?.location?.continent ?? 'No disponible'],
-            ['Código del país', data?.location?.country_code ?? 'No disponible'],
-            ['Latitud', data?.location?.latitude ?? 'No disponible'],
-            ['Longitud', data?.location?.longitude ?? 'No disponible'],
-            ['Zona horaria', data?.location?.time_zone ?? 'No disponible'],
-            ['Autonomous System Number (ASN)', data?.network?.autonomous_system_number ?? 'No disponible'],
-            ['Autonomous System Organization (ASO)', data?.network?.autonomous_system_organization ?? 'No disponible'],
-          ];
-          const output = table(outputTable);
-          const successMessage = '\nInformación agregada a la base de datos.';
-          interaction.reply(`\`\`\`${output}${successMessage}\`\`\``);
+          const successMessage = `\nInformación agregada a la base de datos:\n${output}`;
+          interaction.reply(successMessage);
         });
       } else {
         interaction.reply(`Error en la solicitud. Código de error: ${response.status}`);
